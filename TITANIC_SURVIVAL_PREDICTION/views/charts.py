f6605a7c043691c752f77562e0d6b8d00cb079e6
agastyashah96-ops <agastyashah96@gmail.com>
2026-06-17 15:19:54 +0530

    Update charts.py

--- TITANIC_SURVIVAL_PREDICTION/views/charts.py
+++ TITANIC_SURVIVAL_PREDICTION/views/charts.py
@@ -10,11 +10,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model import data_cleaning
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-
 df = data_cleaning()
 
 with st.container(border=True):
-    st.subheader("Survival Rate by Passenger Class", text_alignment='center')
+    st.subheader("Survival Rate by Passenger Class")
     pclass_df = df.groupby(['Pclass', 'Survived']).size().reset_index(name='Count')
     pclass_df['Survival Status'] = pclass_df['Survived'].map({0: 'Died', 1: 'Survived'})
     st.bar_chart(
@@ -23,10 +22,10 @@ with st.container(border=True):
         y="Count",
         color="Survival Status"
     )
-    st.caption(":rainbow[First class passengers had a much higher survival rate than second or third class.]", text_alignment='center')
+    st.caption(":rainbow[First class passengers had a much higher survival rate than second or third class.]")
 
-with st.container(border=True, gap='small'):
-    st.subheader('Survival rate by sex', text_alignment='center')
+with st.container(border=True):
+    st.subheader('Survival rate by sex')
     sex_df = df.groupby(["Sex", "Survived"]).size().reset_index(name='Count')
     sex_df['Survival Status'] = sex_df['Survived'].map({0: 'Died', 1: 'Survived'})
     sex_df['Sex'] = sex_df['Sex'].map({1: 'Males', 0: 'Female'})
@@ -36,13 +35,13 @@ with st.container(border=True, gap='small'):
         y="Count",
         color="Survival Status"
     )
-    st.caption(":rainbow[Women were more likely to survive the titanic sex and survival has the most positive correlation]", text_alignment='center')
+    st.caption(":rainbow[Women were more likely to survive the titanic sex and survival has the most positive correlation]")
 
 with st.container(border=True):
-    st.subheader('Survival rate by place of embarkment', text_alignment='center')
-    raw_df = pd.read_csv(os.path.join(base_dir, '..', 'TITANIC.csv'))  # fixed path
+    st.subheader('Survival rate by place of embarkment')
+    raw_df = pd.read_csv(os.path.join(base_dir, '..', 'TITANIC.csv'))
     emb_df = raw_df.groupby(['Embarked', 'Survived']).size().reset_index(name='Count')
     emb_df['Survival Status'] = emb_df['Survived'].map({0: 'Died', 1: 'Survived'})
     st.bar_chart(emb_df, x="Embarked", y="Count", color="Survival Status")
     st.caption("C = Cherbourg, Q = Queenstown, S = Southampton")
-    st.caption(":rainbow[The Passengers embarking from Southhampton were generally wealthier which correlated with their survival]",
+    st.caption(":rainbow[The Passengers embarking from Southampton were generally wealthier which correlated with their survival]")
